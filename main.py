ee7e6d677e971a9525eb0b3addc3849dfb98454c
Korhan Mutludoğan <korhanmd@gmail.com>
2023-05-02 11:12:51 +0200

    Add mediapipe solutions

--- main.py
+++ main.py
@@ -2,26 +2,29 @@ import cv2
 import mediapipe as mp
 
 if __name__ == '__main__':
+	mp_drawing  = mp.solutions.drawing_utils
+	mp_pose = mp.solutions.pose
 	cap = cv2.VideoCapture(0)
 
 	if not cap.isOpened():
 		print("Cannot open camera")
 		exit()
 
-	while True:
-		# Capture frame-by-frame
-		ret, frame = cap.read()
+	with mp_pose.Pose(min_detection_confidence = 0.5, min_tracking_confidence = 0.5) as pose:
+		while True:
+			# Capture frame-by-frame
+			ret, frame = cap.read()
 
-		# If frame is read correctly ret is True
-		if not ret:
-			print("Can't receive frame. Exiting...")
-			break
+			# If frame is read correctly ret is True
+			if not ret:
+				print("Can't receive frame. Exiting...")
+				break
 
-		# Display the frame
-		cv2.imshow('frame', frame)
+			# Display the frame
+			cv2.imshow('frame', frame)
 
-		if cv2.waitKey(1) == ord('q'):
-			break
+			if cv2.waitKey(1) == ord('q'):
+				break
 
-	cap.release()
-	cv2.destroyAllWindows()
+		cap.release()
+		cv2.destroyAllWindows()
