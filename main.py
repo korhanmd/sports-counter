648b7f27653f121dfc36dfa6d792a7d96697863c
Korhan Mutludoğan <korhanmd@gmail.com>
2023-05-08 16:44:00 +0200

    Calculate the angle between landmarks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import cv2
 import mediapipe as mp
+import helper_functions
 
 if __name__ == '__main__':
 	mp_drawing  = mp.solutions.drawing_utils
@@ -24,12 +25,23 @@ if __name__ == '__main__':
 				print("Can't receive frame. Exiting...")
 				break
 
+			try:
+				landmarks = results.pose_landmarks.landmark
+
+				shoulder_left = [landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].x, landmarks[mp_pose.PoseLandmark.LEFT_SHOULDER.value].y]
+				wrist_left = [landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].x, landmarks[mp_pose.PoseLandmark.LEFT_WRIST.value].y]
+				wrist_right = [landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].x, landmarks[mp_pose.PoseLandmark.RIGHT_WRIST.value].y]
+
+				angle = helper_functions.calculate_angle(wrist_left, shoulder_left, wrist_right)
+				print(angle)
+			except:
+				pass
+
 			mp_drawing.draw_landmarks(frame, results.pose_landmarks, mp_pose.POSE_CONNECTIONS,
                                 mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=2),
                                 mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
                                 )
-
-
+			
 			# Display the frame
 			cv2.imshow('frame', frame)
 
