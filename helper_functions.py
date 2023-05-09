bf4a8200cb11e085228ccf0a4c6eb26cf644c027
Korhan Mutludoğan <korhanmd@gmail.com>
2023-05-09 15:49:08 +0200

    Get rid of trailing whitespaces

--- helper_functions.py
+++ helper_functions.py
@@ -8,19 +8,20 @@ def calculate_angle(p1, p2, p3):
 def write_angle(image, angle):
 	# font
     font = cv2.FONT_HERSHEY_SIMPLEX
-			  
+    
 	# org
     org = (50, 50)
-	  
+    
 	# fontScale
     fontScale = 1
-	   
+    
 	# Blue color in BGR
     color = (255, 0, 0)
-	  
+    
 	# Line thickness of 2 px
     thickness = 2
-	   
+    
 	# Using cv2.putText() method
-    cv2.putText(image, "{0:.2f}".format(angle), org, font, 
+    cv2.putText(image, "{0:.2f}".format(angle), org, font,
                 fontScale, color, thickness, cv2.LINE_AA)
+    
