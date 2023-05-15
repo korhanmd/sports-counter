7a76a6b3e3735990f9c7323832b9ffd9f5c4dc5b
Korhan Mutludoğan <korhanmd@gmail.com>
2023-05-15 10:30:58 +0200

    Create unit tests

---
+++ test_script.py
@@ -0,0 +1,20 @@
+from helper_functions import *
+
+class TestCalculateAngle:
+    def test_1(self):
+        assert calculate_angle([-1,0],[0,0],[1,0]) == 180.0
+
+    def test_2(self):
+        assert calculate_angle([-1,0],[0,0],[10,0]) == 180.0
+
+    def test_3(self):
+        assert calculate_angle([-1,0],[0,0],[1,1]) == 225.0
+
+    def test_4(self):
+        assert calculate_angle([1,0],[0,0],[1,0]) == 0.0
+
+    def test_5(self):
+        assert calculate_angle([1,-1],[0,0],[1,0]) == 45.0
+
+    def test_6(self):
+        assert calculate_angle([1,-1],[0,0],[1,1]) == 90.0
